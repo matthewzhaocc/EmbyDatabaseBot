4376df48dbfc75b845505129b0005ad33b44ab43
Matthew Zhao <matthew@mail.matthew-cloud.com>
2020-03-09 23:33:51 -0700

    add proper os based import checking

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
-try:
+import sys
+if sys.platform != "nt":
     # uvloop doesn't work on Windows, therefore an optional dependency
     import uvloop
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-except ImportError:
-    pass
 
 import mandrake
 
